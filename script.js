43a443780c6edce72559cacc7c91e4182e608c6c
Dalla Valle Cassiano <145142163+DallavalleCassiano@users.noreply.github.com>
2025-04-01 08:25:56 -0300

    2025/04/01

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-document. addEventListener('DOMContentLoaded',function(){
-const aumentaFOnteBotao = documentElementById('aumentar-fonte')
-let tamanhoAtualFonte= 1;
-}
-)
+document.addEventListener('DOMContentLoaded', function () {
+    const aumentaFOnteBotao = documentElementById('aumentar-fonte');
+
+    let tamanhoAtualFonte = 1;
+    
+    aumentaFOnteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte += 0,1;
+        document.body.style.fontSize = `${ tamanhoAtualFonte } rem`;
+    };
+        
+
+});
